08e0c1864c2f018ef3429dd6b43a0c75ff7048cd
Jakub <johncenajakub@gmail.com>
2019-05-28 12:06:45 +0200

    deploy test

--- src/main/java/com/example/test/Adder.java
+++ src/main/java/com/example/test/Adder.java
@@ -1,5 +1,11 @@
 package com.example.test;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class Adder {
 
     public int add(int number1, int number2) {
